5f8747bb8b8b9079310a93d77fbb7196b0abc263
loupers <loupers@matfyz.cz>
2022-11-18 14:43:53 +0100

    server first working version

--- server/server.go
+++ server/server.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"auctionSystem/grpc"
+	"context"
 	"log"
 	"math"
-	"math/rand"
 	"net"
 	"os"
 	"strconv"
@@ -17,7 +17,7 @@ import (
 type Server struct {
 	auctionSystem.UnimplementedAuctionServer
 	name string
-	port int
+	port int64
 	// Store a progressive number for the Ids of the clients in order to give a univoque id to each client
 	idCounter  int64
 	clients    []int64
@@ -26,20 +26,23 @@ type Server struct {
 }
 
 type HighestBid struct {
-	clientId      int64
-	item          AuctionItem
-	auctionLength int64
-	startTime     time.Time
+	clientId  int64
+	item      AuctionItem
+	startTime time.Time
 }
 
 type AuctionItem struct {
-	name string
-	bid  int64
+	name          string
+	bid           int64
+	auctionLength int64
 }
 
 var auctionItems = [...]AuctionItem{
-	{name: "Item1", bid: 50},
-	{name: "Item2", bid: 20},
+	{name: "Item1", bid: 50, auctionLength: 20},
+	{name: "Item2", bid: 20, auctionLength: 45},
+	{name: "Item3", bid: 20, auctionLength: 45},
+	{name: "Item4", bid: 20, auctionLength: 45},
+	{name: "Item5", bid: 20, auctionLength: 45},
 }
 
 // Add this part if we want to use parametric port on call of the method
@@ -48,10 +51,9 @@ var auctionItems = [...]AuctionItem{
 func main() {
 	// Get the port from the command line when the server is run
 	// flag.Parse()
-
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
 	// Hardcoded port
-	port_value := 8080
-	port := &port_value
+	port := &arg1
 
 	// Create a server struct
 	server := &Server{
@@ -71,7 +73,7 @@ func startServer(server *Server) {
 	grpcServer := grpc.NewServer()
 
 	// Make the server listen at the given port (convert int port to string)
-	list, err := net.Listen("tcp", ":"+strconv.Itoa(server.port))
+	list, err := net.Listen("tcp", ":"+strconv.Itoa(int(server.port)))
 
 	if err != nil {
 		log.Fatalf("Could not create the server %v", err)
@@ -88,33 +90,34 @@ func startServer(server *Server) {
 
 	log.Printf("Started server at port: %d\n", server.port)
 
+	go server.updateBids()
+
 	// Register the grpc server and serve its listener
 	auctionSystem.RegisterAuctionServer(grpcServer, server)
 	serveError := grpcServer.Serve(list)
 	if serveError != nil {
 		log.Fatalf("Could not serve listener")
 	}
+
 }
 
 func (s *Server) updateBids() {
 	numOfItems := len(auctionItems)
 	counter := 0
-
-	minAuctionLenght := 20
-	maxAuctionLength := 60
 	for {
 		s.mutex.Lock()
 		s.currentBid.item = auctionItems[counter%numOfItems]
 		s.currentBid.startTime = time.Now()
-		s.currentBid.auctionLength = int64(rand.Intn(maxAuctionLength-minAuctionLenght) + minAuctionLenght)
+		log.Printf("Auction for item %v started at %v and lasts %v seconds\n", s.currentBid.item.name, s.currentBid.startTime, s.currentBid.item.auctionLength)
 		s.mutex.Unlock()
 
-		time.Sleep(time.Duration(s.currentBid.auctionLength) * time.Second)
+		time.Sleep(time.Duration(s.currentBid.item.auctionLength) * time.Second)
 		counter++
+		log.Printf("Auction for item %v ended at %v\n", s.currentBid.item.name, time.Now())
 	}
 }
 
-func (s *Server) Bid(request auctionSystem.BidRequest) auctionSystem.BidReply {
+func (s *Server) Bid(ctx context.Context, request *auctionSystem.BidRequest) (*auctionSystem.BidReply, error) {
 	var id int64
 	var success bool
 	var bestBid int64
@@ -138,18 +141,20 @@ func (s *Server) Bid(request auctionSystem.BidRequest) auctionSystem.BidReply {
 		s.currentBid.item.bid = request.Amount
 		s.currentBid.clientId = request.ClientId
 		bestBid = request.Amount
+		log.Printf("Bid %v from client %v accepted, current bid is %v\n", request.Amount, request.ClientId, bestBid)
 	} else {
 		success = false
 		s.mutex.RLock()
 		defer s.mutex.RUnlock()
 
 		bestBid = s.currentBid.item.bid
+		log.Printf("Bid %v from client %v declined, current bid is %v\n", request.Amount, request.ClientId, bestBid)
 	}
 
-	return auctionSystem.BidReply{ClientId: id, Success: success, BestBid: bestBid}
+	return &auctionSystem.BidReply{ClientId: id, Success: success, BestBid: bestBid}, nil
 }
 
-func (s *Server) Show(request auctionSystem.ShowRequest) auctionSystem.ShowReply {
+func (s *Server) Show(ctx context.Context, request *auctionSystem.ShowRequest) (*auctionSystem.ShowReply, error) {
 	var secondsLeft int64
 	var currentBid int64
 	var winningClient int64
@@ -163,10 +168,12 @@ func (s *Server) Show(request auctionSystem.ShowRequest) auctionSystem.ShowReply
 	currentBid = s.currentBid.item.bid
 	secondsLeft = s.getSecondsTillEnd()
 
-	return auctionSystem.ShowReply{CurrentBid: currentBid, WinningClientId: winningClient, ObjectName: name, SecondsTillEnd: secondsLeft}
+	log.Printf("Show request received, current bid %v", currentBid)
+
+	return &auctionSystem.ShowReply{CurrentBid: currentBid, WinningClientId: winningClient, ObjectName: name, SecondsTillEnd: secondsLeft}, nil
 }
 
 func (s *Server) getSecondsTillEnd() int64 {
 	diff := time.Now().Sub(s.currentBid.startTime)
-	return s.currentBid.auctionLength - int64(math.Round(diff.Seconds()))
+	return s.currentBid.item.auctionLength - int64(math.Round(diff.Seconds()))
 }
